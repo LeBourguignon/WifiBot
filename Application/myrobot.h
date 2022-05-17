9740056df3547aaf9d10a2ad726e7a057f05bb83
LeBourguignon <63340452+LeBourguignon@users.noreply.github.com>
2022-05-17 09:32:05 +0200

    Import MyRobot

--- Application/myrobot.h
+++ Application/myrobot.h
@@ -2,15 +2,34 @@
 #define MYROBOT_H
 
 #include <QObject>
+#include <QTcpSocket>
+#include <QAbstractSocket>
+#include <QDebug>
+#include <QTimer>
+#include <QMutex>
 
-class MyRobot : public QObject
-{
+class MyRobot : public QObject {
     Q_OBJECT
 public:
-    explicit MyRobot(QObject *parent = nullptr);
+    explicit MyRobot(QObject *parent = 0);
+    void doConnect();
+    void disConnect();
+    QByteArray DataToSend;
+    QByteArray DataReceived;
+    QMutex Mutex;
 
 signals:
+    void updateUI(const QByteArray Data);
+public slots:
+    void connected();
+    void disconnected();
+    void bytesWritten(qint64 bytes);
+    void readyRead();
+    void MyTimerSlot();
 
+private:
+    QTcpSocket *socket;
+    QTimer *TimerEnvoi;
 };
 
 #endif // MYROBOT_H
