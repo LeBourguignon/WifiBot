1bd0ef11ef3cb7b5e6b37a685da8b1666fcc8027
LeBourguignon <63340452+LeBourguignon@users.noreply.github.com>
2022-06-01 11:25:20 +0200

    Up class MyRobot
    + isConnect
    + move()
    + crc16()

--- Application/myrobot.h
+++ Application/myrobot.h
@@ -8,28 +8,39 @@
 #include <QTimer>
 #include <QMutex>
 
+#include "direction.h"
+
 class MyRobot : public QObject {
     Q_OBJECT
 public:
     explicit MyRobot(QObject *parent = 0);
     void doConnect();
     void disConnect();
+
     QByteArray DataToSend;
     QByteArray DataReceived;
+
     QMutex Mutex;
+
+    bool isConnect;
 
 signals:
     void updateUI(const QByteArray Data);
 public slots:
     void connected();
     void disconnected();
+
     void bytesWritten(qint64 bytes);
     void readyRead();
     void MyTimerSlot();
 
+    void move(Direction direction = Direction::NONE, quint8 Velocity = 0x00);
+
 private:
     QTcpSocket *socket;
     QTimer *TimerEnvoi;
+
+    quint16 crc16(QByteArray adresseTab, unsigned int tailleMax);
 };
 
 #endif // MYROBOT_H
