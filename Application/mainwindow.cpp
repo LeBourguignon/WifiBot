8e682b52067973eec8efa63ff4891c37b6423aa2
Inès Amoura <95071284+inesamoura@users.noreply.github.com>
2022-06-14 13:58:14 +0200

    creation de l'interface garphique sans couleur et sans adaptation à la taille de la fenêtre
    Dans cette seconde version de l'interface graphique j'ai finalisé la structure globale et finale

--- Application/mainwindow.cpp
+++ Application/mainwindow.cpp
@@ -8,8 +8,6 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    camera = new Camera(this);
-    setStyleSheet("background-color: rgb(200,200,200)");
 }
 
 MainWindow::~MainWindow()
