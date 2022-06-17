a6f496d0854ee21941923c4479cf12686275a8c3
Inès Amoura <95071284+inesamoura@users.noreply.github.com>
2022-06-17 15:20:13 +0200

    interface finale
    nouvelle image
    nouveau bouton
    bon dimensionnement

--- Application/main.cpp
+++ Application/main.cpp
@@ -6,10 +6,9 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    w.setStyleSheet("background-image:url(:Fond3.png)");
+    w.setStyleSheet("background-image:url(:Fond2.png)");
     w.show();
     //w.showFullScreen();
 
     return a.exec();
 }
-
